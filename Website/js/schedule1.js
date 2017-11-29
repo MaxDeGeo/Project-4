fc6fcd05e1eb3f094288df754b49a566c0443ea3
Max De George <maxtdegeorge@gmail.com>
2017-11-29 15:01:45 -0500

    Added formatting to table, dates, and time spots

--- Website/js/schedule1.js
+++ Website/js/schedule1.js
@@ -3,7 +3,7 @@ window.onload = function() {
 }
 
 function ColorCreator() {
-    let cardArray = document.querySelectorAll("div .col")
+    let cardArray = document.querySelectorAll("#cards-content div .col")
     let tempValue = 0;
 
 
